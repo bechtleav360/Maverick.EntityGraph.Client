57fd4a09bf21ee4e0c1cdb3d76806e035fd3db3e
mumi <m.polatkan@outlook.de>
2023-08-15 13:29:53 +0200

    fix

--- entitygraph/bulk_builder.py
+++ entitygraph/bulk_builder.py
@@ -17,7 +17,7 @@ class BulkBuilder:
         tmp = ''
         for entity_builder in self.entity_builders:
             tmp += entity_builder.graph.serialize(format='turtle')
-        print(tmp)
+
         endpoint = f'api/entities'
         headers = {'X-Application': self._application_label, 'Content-Type': 'text/turtle', 'Accept': 'text/turtle'}
         entitygraph._base_client.make_request('POST', endpoint, headers=headers, data=tmp)
